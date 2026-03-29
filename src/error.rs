6353c9ef125d02a61cf096faca02c6087af34995
Piotr Nowakowski <kontakt@piotreknow.dev>
2026-03-30 00:16:42 +0200

    Cleanup for error enum

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,6 @@ use crate::inference::error::Error as InferenceError;
 use crate::model::error::Error as ModelError;
 
 #[derive(Error, Debug)]
-#[allow(dead_code)]
 pub enum Error {
     #[error("Model error: {0}")]
     ModelError(#[from] ModelError),
@@ -15,15 +14,6 @@ pub enum Error {
     #[error("Web UI error: {0}")]
     WebUIError(String),
 
-    #[error("Image error: {0}")]
-    ImageError(#[from] image::ImageError),
-
-    #[error("ORT error: {0}")]
-    ORTError(#[from] ort::Error),
-
     #[error("Model not found: {0}")]
     ModelNotFound(String),
-
-    #[error("Output format does not support transparency. Use PNG, WebP, GIF, TIFF, or ICO.")]
-    OutputFormatError,
 }
